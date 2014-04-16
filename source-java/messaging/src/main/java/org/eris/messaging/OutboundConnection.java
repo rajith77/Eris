5f7bb58b17db7d94a2d754e7270b0ff364433f9f
Rajith Attapattu <rajith77@gmail.com>
2014-04-16 13:43:47 -0400

    Checking in some initial ideas on the server side API.

---
+++ source-java/messaging/src/main/java/org/eris/messaging/OutboundConnection.java
@@ -0,0 +1,60 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.eris.messaging;
+
+/**
+ * 
+ * Represents a logical <i>Connection</i> to a remote peer within a messaging
+ * network.
+ * 
+ * <h4>Exceptions</h4>
+ * <ul>
+ * <li>TransportException : Thrown when the underlying transport fails.</li>>
+ * <li>ReceiverException  : Thrown when the Connection gets to an erroneous state.</li>
+ * <li>TimeoutException   : Thrown when an operation exceeds the connection timeout.</li>
+ * </ul>
+ * 
+ * Connection timeout defaults to 60 secs. This value can be changed via the
+ * <i>"eris.connection.timeout"</i> system property, or by providing an
+ * application specific ConnectionSettings implementation when creating the
+ * Connection object.
+ * 
+ * @see ConnectionSettings
+ */
+public interface OutboundConnection
+{
+    /**
+     * Creates the underlying physical connection to the peer.
+     */
+    public void connect() throws TransportException, ConnectionException, TimeoutException;
+
+    /**
+     * Establishes a logical Session for exchanging of messages.
+     */
+    public Session createSession() throws TransportException, ConnectionException, TimeoutException;
+
+    /**
+     * Terminates the Connection and free any resources associated with this
+     * Connection. If there are any active sessions, it will close them first
+     * before closing the Connection.
+     */
+    public void close() throws TransportException, ConnectionException, TimeoutException;
+}
